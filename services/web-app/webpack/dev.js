91b5d758c7c28f81157f8930bf8ef9716fdd16be
shsangha <sssangha@ucalgary.ca>
2018-10-04 10:28:55 -0600

    initial webpack dev setup for docker

---
+++ services/web-app/webpack/dev.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.[hash].js',
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'),
+      filename: 'index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    hot: true,
+  },
+  watch: true,
+  watchOptions:{
+    poll: 1000
+  }
+};
