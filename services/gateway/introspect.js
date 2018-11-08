9e637e70c833770aeecbbe1fb56c7be1f2e017ea
shsangha <sssangha@ucalgary.ca>
2018-11-07 17:33:18 -0700

    deleted the create link function as it was being called 3 times for no reason, got forwarding of context to remote schemas working. had to add in a check for when the servier initilzed as there is no context object to be passed at that point

--- services/gateway/introspect.js
+++ services/gateway/introspect.js
@@ -1,18 +1,47 @@
 const { makeRemoteExecutableSchema, introspectSchema } = require('graphql-tools');
-const { createHttpLink } = require('apollo-link-http');
+const { HttpLink } = require('apollo-link-http');
+const { setContext } = require('apollo-link-context');
 const fetch = require('node-fetch');
 
 module.exports = {
-  getIntrospectedSchema: async (uri) => {
-    const createLink = () => createHttpLink({
+  getIntrospectedSchema: async uri => {
+    const http = new HttpLink({
       uri,
-      fetch,
+      fetch
     });
-    const schema = await introspectSchema(createLink());
+
+    const link = setContext((req, pCtx) => {
+      if (
+        pCtx.graphqlContext &&
+        pCtx.graphqlContext.hasOwnProperty('token') &&
+        pCtx.graphqlContext.hasOwnProperty('refToken')
+      ) {
+        return {
+          headers: {
+            'x-token': pCtx.graphqlContext.token,
+            'x-refresh-token': pCtx.graphqlContext.refToken
+          }
+        };
+      }
+      return {
+        headers: {
+          'x-token': null,
+          'x-refresh-token': null
+        }
+      };
+    }).concat(http);
+    const schema = await introspectSchema(link);
 
     return makeRemoteExecutableSchema({
       schema,
-      link: createLink(),
+      link
     });
-  },
+  }
 };
+/*
+const createLink = () =>
+      createHttpLink({
+        uri,
+        fetch
+      });
+*/
