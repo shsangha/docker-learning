0c771deb7d61a2b239446458ef12a873804f32ab
shsangha <sssangha@ucalgary.ca>
2018-11-07 17:31:50 -0700

    now takes in context from the request headers coming in from client

--- services/gateway/index.js
+++ services/gateway/index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { mergeSchemas } = require('graphql-tools');
+const jwt = require('jsonwebtoken');
+const authMiddleware = require('./middlewares/auth');
 const { getIntrospectedSchema } = require('./introspect');
 
 const app = express();
 
-const remoteEndpoints = [
-  'http://localhost:4000/graphql',
-];
+const remoteEndpoints = ['http://localhost:3030/graphql'];
 
-
-(async function startServer() {
+(async function runMergedSchema() {
   try {
-    const allSchemas = await Promise.all(remoteEndpoints.map(ep => getIntrospectedSchema(ep)));
-    const server = new ApolloServer({ schema: mergeSchemas({ schemas: allSchemas }) });
+    const schemas = await Promise.all(remoteEndpoints.map(ep => getIntrospectedSchema(ep)));
+    const server = new ApolloServer({
+      schema: mergeSchemas({ schemas }),
+      context: ({ req }) => ({
+        token: req.headers['x-token'],
+        refToken: req.headers['x-refresh-token']
+      })
+    });
+    //  app.use(authMiddleware);
     server.applyMiddleware({ app });
-    app.listen({ port: 3030 }, () => {
-      console.log('gateway at 3030');
+    app.listen({ port: 4000 }, () => {
+      console.log('gateway at 4000 up and running');
     });
   } catch (e) {
     console.error(e);
   }
-}());
+})();
