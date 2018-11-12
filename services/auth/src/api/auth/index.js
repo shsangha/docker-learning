1c13284adc8b1b416df45e9c8f724d2f9f115342
shsangha <sssangha@ucalgary.ca>
2018-11-11 20:45:42 -0700

    simple compose file to test auth service on its own

--- services/auth/src/api/auth/index.js
+++
@@ -1,9 +0,0 @@
-const typeDefs = require('./auth.schema');
-const resolvers = require('./auth.resolvers');
-const model = require('./auth.model');
-
-module.exports = {
-  resolvers,
-  typeDefs,
-  model,
-};
--- services/web-app/src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
-
-
-const App = () => (
-  <div>React</div>
-);
-
-export default hot(module)(App);
