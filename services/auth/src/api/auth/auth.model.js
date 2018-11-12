d9269f3c22587f1b6478120b0f4447f0434557a7
shsangha <sssangha@ucalgary.ca>
2018-11-11 20:42:46 -0700

    revised validator

--- services/auth/src/api/auth/auth.model.js
+++ services/auth/src/api/auth/auth.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const { Schema } = mongoose;
 
@@ -7,12 +8,34 @@ const UserSchema = new Schema({
     type: String,
     required: [true, 'Email is required'],
     unique: true,
+    validate: {
+      validator: function(v) {
+        return  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+      },
+      message: 'Enter a valid email',
+    },
   },
   password: {
     type: String,
     required: [true, 'Password is required'],
+    validate: {
+      validator: function(v) {
+        return  /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8})$/.test(v);
+      },
+      message: 'Password requires 1 number, 1 uppercase character and 8 characters',
+    },
   },
 });
 
+UserSchema.pre('save', function (next) {
+  let user = this;
+   if (!user.isModified('password')) return next;
+
+   bcrypt.hash(user.password,10, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+   });
+});
 
 module.exports = mongoose.model('User', UserSchema);
