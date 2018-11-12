3a0517fad835ec29a6f21a63fceb33566cb91fa8
shsangha <sssangha@ucalgary.ca>
2018-11-11 20:39:38 -0700

    got middleware working to request refresh tokens from the auth service when needed

--- services/gateway/middlewares/auth.js
+++ services/gateway/middlewares/auth.js
@@ -1,22 +1,36 @@
 const jwt = require('jsonwebtoken');
 const fetch = require('node-fetch');
 
+const { SECRET } = process.env;
+
 module.exports = async (req, res, next) => {
   const token = req.headers['x-token'];
+
   if (token) {
     try {
       const { user } = jwt.verify(token, SECRET);
       req.user = user;
-    } catch (error) {
+      next();
+    } catch (err) {
       const refreshToken = req.headers['x-refresh-token'];
-      const newTokens = await fetch('AUTHURL/refresh', { method: 'POST', body: { token, refreshToken } });
-      if (newTokens && newTokens.refreshToken) {
-        res.set('Access-Control-Expose-Heasers', 'x-token', 'x-refresh-token');
-        res.set('x-token', newTokens.token);
-        res.set('x-refresh-token', newTokens.refreshToken);
-      }
-      req.user = newTokens.user;
+      fetch('http://auth-service:3030/refreshtokens', {
+        method: 'POST',
+        body: JSON.stringify({
+          refreshToken
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(data => data.json())
+        .then(data => {
+          res.set('Access-Control-Expose-Headers', 'x-token', 'x-refresh-token');
+          res.set('x-token', data.token);
+          res.set('x-refresh-token', data.refreshToken);
+          req.user = data.user;
+        })
+        .then(data => next())
+        .catch(e => next());
     }
   }
-  next();
 };
