b80c289d2a1d1976ecb144151ea3063901931d95
shsangha <sssangha@ucalgary.ca>
2018-11-13 21:59:11 -0700

    moved to simplfy testing and mocking

---
+++ services/auth/src/api/auth/auth.helpers/refreshTokens.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const User = require('./auth.model');
+
+const { SECRET, REF_SECRET } = process.env;
+
+module.exports = async refreshToken => {
+  let userId;
+  //  console.log(jwt.decode(refreshToken));
+  try {
+    const {
+      user: { id }
+    } = jwt.decode(refreshToken);
+    userId = id;
+  } catch (e) {
+    return {};
+  }
+  if (!userId) {
+    return {};
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return {};
+  }
+  const refreshSecret = user.password + REF_SECRET;
+  try {
+    jwt.verify(refreshToken, refreshSecret);
+  } catch (err) {
+    return {};
+  }
+
+  try {
+    const { token, refreshToken } = createTokens(user);
+    return {
+      token,
+      refreshToken,
+      user
+    };
+  } catch (error) {
+    return {};
+  }
+};
