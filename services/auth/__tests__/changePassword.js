359f74355c31734f3d01e5cb04e2e41a72937c6a
shsangha <sssangha@ucalgary.ca>
2018-11-14 18:36:36 -0700

    test to change password and return new tokens

---
+++ services/auth/__tests__/changePassword.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+const { gql } = require('apollo-server');
+const { createTestClient } = require('apollo-server-testing');
+const User = require('../src/api/auth/auth.model');
+const createServer = require('../test-helpers/createServer');
+
+const changepwMutation = gql`
+  mutation changpw($input: String!) {
+    changePassword(password: $input) {
+      ... on error {
+        passwordErrors
+      }
+      ... on success {
+        token
+        refreshToken
+      }
+    }
+  }
+`;
+
+const validEmail = 'shawn@pwchangetest.com';
+const validPassword = '5%%fds9pf8sdf3';
+
+jest.mock('../src/api/auth/auth.helpers/createTokens', () => user => {
+  return { token: 'token', refreshToken: 'refreshToken' };
+});
+describe('testing change pw functionality', () => {
+  beforeAll(async () => {
+    await mongoose.connect(global.__MONGO_URI__);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  beforeEach(async () => {
+    await User.remove({ email: validEmail });
+  });
+
+  test('catches if we try to change to an invalid pw', async () => {
+    const user = await User.create({
+      email: validEmail,
+      password: validPassword
+    });
+
+    const server = createServer({
+      models: {
+        User
+      },
+      user: { id: user._id, email: user.email }
+    });
+
+    const { mutate } = createTestClient(server);
+
+    const { data, errors } = await mutate({
+      mutation: changepwMutation,
+      variables: { input: 'invalidpw' }
+    });
+    expect(data.changePassword.passwordErrors).toContain('invalid password');
+  });
+  test('return new tokens on a valid password change', async () => {
+    const user = await User.create({
+      email: validEmail,
+      password: validPassword
+    });
+    const server = createServer({
+      models: {
+        User
+      },
+      user: { id: user._id, email: user.email }
+    });
+
+    const { mutate } = createTestClient(server);
+
+    const { data, errors } = await mutate({
+      mutation: changepwMutation,
+      variables: { input: '%hFh43*fdsdf' }
+    });
+
+    expect(data.changePassword.token).toEqual('token');
+    expect(data.changePassword.refreshToken).toEqual('refreshToken');
+  });
+});
