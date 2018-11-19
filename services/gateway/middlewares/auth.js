0ae79a30d65fd05fc31c84132d84cae33be6178b
shsangha <sssangha@ucalgary.ca>
2018-11-18 19:30:03 -0700

    fixed race condition issue where resolvers were returning before middleware finished

--- services/gateway/middlewares/auth.js
+++ services/gateway/middlewares/auth.js
@@ -7,30 +7,34 @@ module.exports = async (req, res, next) => {
   const token = req.headers['x-token'];
 
   if (token) {
+    console.log('running the inside');
     try {
-      const { user } = jwt.verify(token, SECRET);
+      const { user } = await jwt.verify(token, SECRET);
       req.user = user;
       next();
     } catch (err) {
       const refreshToken = req.headers['x-refresh-token'];
-      fetch('http://auth-service:3030/refreshtokens', {
-        method: 'POST',
-        body: JSON.stringify({
-          refreshToken
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(data => data.json())
-        .then(data => {
+      try {
+        const data = await fetch('http://auth-service:3030/refreshtokens', {
+          method: 'POST',
+          body: JSON.stringify({
+            refreshToken
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const re = await data.json();
+        if (Object.keys(re).length !== 0) {
           res.set('Access-Control-Expose-Headers', 'x-token', 'x-refresh-token');
           res.set('x-token', data.token);
           res.set('x-refresh-token', data.refreshToken);
-          req.user = data.user;
-        })
-        .then(data => next())
-        .catch(e => next());
+          req.user = re.user;
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
+  next();
 };
