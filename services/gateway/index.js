0c6caccb9e3f7fbd6023abfc445d5fed72462ecf
shsangha <sssangha@ucalgary.ca>
2018-11-19 12:38:56 -0700

    exposed custom headers to be intercepted in client side afterware

--- services/gateway/index.js
+++ services/gateway/index.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { mergeSchemas } = require('graphql-tools');
-const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const authMiddleware = require('./middlewares/auth');
 const { getIntrospectedSchema } = require('./introspect');
 
 const app = express();
 
-const remoteEndpoints = ['http://localhost:3030/graphql'];
+const remoteEndpoints = ['http://auth-service:3030/graphql'];
 
 (async function runMergedSchema() {
   try {
     const schemas = await Promise.all(remoteEndpoints.map(ep => getIntrospectedSchema(ep)));
     const server = new ApolloServer({
       schema: mergeSchemas({ schemas }),
-      context: ({ req }) => ({
-        token: req.headers['x-token'],
-        refToken: req.headers['x-refresh-token']
-      })
+      context: ({ req }) => {
+        return {
+          user: req.user
+        };
+      }
     });
-    //  app.use(authMiddleware);
+    app.use(
+      cors({
+        exposedHeaders: ['x-token', 'x-refresh-token']
+      })
+    );
+    app.use(authMiddleware);
     server.applyMiddleware({ app });
     app.listen({ port: 4000 }, () => {
       console.log('gateway at 4000 up and running');
