5de9a652bc6b91b06be523b5112affcec23a25fb
shsangha <sssangha@ucalgary.ca>
2018-11-18 20:02:32 -0700

    fixed bug when the token was valid,didnt need to be calling next

--- services/gateway/middlewares/auth.js
+++ services/gateway/middlewares/auth.js
@@ -7,15 +7,13 @@ module.exports = async (req, res, next) => {
   const token = req.headers['x-token'];
 
   if (token) {
-    console.log('running the inside');
     try {
       const { user } = await jwt.verify(token, SECRET);
       req.user = user;
-      next();
     } catch (err) {
       const refreshToken = req.headers['x-refresh-token'];
       try {
-        const data = await fetch('http://auth-service:3030/refreshtokens', {
+        const authResponse = await fetch('http://auth-service:3030/refreshtokens', {
           method: 'POST',
           body: JSON.stringify({
             refreshToken
@@ -24,12 +22,12 @@ module.exports = async (req, res, next) => {
             'Content-Type': 'application/json'
           }
         });
-        const re = await data.json();
-        if (Object.keys(re).length !== 0) {
+        const data = await authResponse.json();
+        if (Object.keys(data).length !== 0 && data.token && data.refreshToken && data.user) {
           res.set('Access-Control-Expose-Headers', 'x-token', 'x-refresh-token');
           res.set('x-token', data.token);
           res.set('x-refresh-token', data.refreshToken);
-          req.user = re.user;
+          req.user = data.user;
         }
       } catch (error) {
         console.log(error);
