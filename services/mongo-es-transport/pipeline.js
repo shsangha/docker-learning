44471a6a6810b27b8c12480dd29af5db3aef86b8
shsangha <sssangha@ucalgary.ca>
2018-11-28 16:57:30 -0700

    added longer timeout to be more forgiving, still crashes sometimes when it cant find elasticsearch version but that is a bug in the transporter code

--- services/mongo-es-transport/pipeline.js
+++ services/mongo-es-transport/pipeline.js
@@ -1,13 +1,13 @@
 var source = mongodb({
-  uri: 'mongodb://root:root@listingDB:27017/listings?authSource=admin',
+  uri: 'mongodb://root:root@listing-db:27017/listings?authSource=admin',
   tail: true,
-  timeout: '30s',
+  timeout: '60s',
   bulk: false
 });
 
 var sink = elasticsearch({
   uri: 'http://elasticsearch:9200/listings',
-  timeout: '30s'
+  timeout: '60s'
 });
 // prettier-ignore
-t.Source('source', source, '/.*/').Transform(goja({"filename":"priceFilterTransform.js"})).Transform(omit({"fields": ["likes", "description", "photos", "accept_offers", "comments", "sold", "buy_now", "shipping"]})).Save('sink', sink, '/.*/');
+t.Source('source', source, '/.*/').Transform(goja({"filename":"priceFilterTransform.js"})).Transform(omit({"fields": ["description", "photos", "accept_offers", "comments", "buy_now", "shipping"]})).Save('sink', sink, '/.*/');
