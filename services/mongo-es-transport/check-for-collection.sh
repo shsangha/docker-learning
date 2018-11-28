591ea7448a727e8374a04405d089ec9630002289
shsangha <sssangha@ucalgary.ca>
2018-11-28 16:55:56 -0700

    fixed broken if clause to check if collection exists, response from mongo was coming in as a string rather than an array

--- services/mongo-es-transport/check-for-collection.sh
+++ services/mongo-es-transport/check-for-collection.sh
@@ -2,8 +2,12 @@
 
 #mongo wont tail the oplog if there are no collections in the DB, so this this script is needed if using a fresh mongo volume 
 
-status=$(mongo listings --host listingDB:27017 -u root -p root --authenticationDatabase admin  --eval 'db.getCollection('listings')')
-if [ $? -ne 0 ]; then
+res=$(mongo listings --host rs0/listing-db:27017 -u root -p root --authenticationDatabase admin --quiet  --eval 'db.getCollectionNames().indexOf("listings")')
+
+status=${res: -2}
+
+if [[ "$status" == "-1" ]]; then
   # Replicaset not yet configured
-  mongo listings --host rs0/listingDB:27017 -u root -p root --authenticationDatabase admin --eval 'db.createCollection("listings")';
+  mongo listings --host rs0/listing-db:27017 -u root -p root --authenticationDatabase admin --eval 'db.createCollection("listings")';
 fi
+
