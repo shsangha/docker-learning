612db11f59d2a337cacd3bed9e36c0aae7ec965d
shsangha <sssangha@ucalgary.ca>
2019-01-15 15:42:44 -0700

    added custom analyzer, mapped suggestion contexts to sold status, and fixed mappings for other fields

--- services/mongo-es-transport/configure-mapping.sh
+++ services/mongo-es-transport/configure-mapping.sh
@@ -9,7 +9,46 @@ IP=$(curl -I --write-out "%{http_code}" --silent --output /dev/null http://elast
 if [[ "$IP" -eq 404 ]]; then
   
   curl -X PUT "http://elasticsearch:9200/listings" -H 'Content-Type: application/json' -d'
-
+{
+    "settings" : {
+        "index" : {
+            "number_of_shards" : 3, 
+            "number_of_replicas" : 2,
+            "analysis": {
+              "analyzer": {
+                "custom_analyzer": {
+                  "type": "custom",
+                  "tokenizer": "lowercase",
+                  "char_filter": [],
+                  "filter": [
+                    "asciifolding",
+                    "trim"
+                  ]
+                }
+              }
+            }
+        }    
+    },
+    "mappings": {
+      "listings": {
+        "properties": {
+          "name": {"type": "text", "analyzer": "custom_analyzer"},
+          "designer": {"type": "keyword"},
+          "category": {"type": "keyword"},
+          "size": {"type": "keyword"},
+          "price": {"type": "scaled_float", "scaling_factor":100},
+          "location": {"type": "keyword"},
+          "market": {"type": "keyword"},
+          "posted": {"type": "date"},
+          "views": {"type": "integer"},
+          "wanted": {"type": "integer"},
+          "designer_suggestions": {"type": "completion","contexts": [{"name": "status", "type": "category", "path": "status"}] , "analyzer": "simple"},
+          "name_suggestions": {"type": "completion", "contexts": [{"name": "status", "type": "category", "path": "status"}] ,"analyzer": "stop"}
+        }
+      }
+    }
+}
+'
 elif [[ "$IP" -eq 200 ]]; then
   echo "INDEX EXISTS DOING NOTHING"
 
