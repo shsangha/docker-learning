dfebc252ba8a1b76bcaef91a3c21984c4a6decc9
shsangha <sssangha@ucalgary.ca>
2019-01-15 16:12:21 -0700

    realized on changes mongo doesn't provide the full object so i needed to remove the top if statement which was causing updates to the completion suggester inputs to be ingored

--- services/mongo-es-transport/indexSuggestions.js
+++ services/mongo-es-transport/indexSuggestions.js
@@ -2,18 +2,24 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable dot-notation */
 
+/* function to add the suggestion options to es since there is not need to store them in mongo
+  @param {Object} [doc] - the changes object coming in from Mongo
+  @returns {Object} the changes object with suggestions appended for elasticsearch
+  
+*/
+
 var suggestions;
 function transform(doc) {
-  if (doc['data']['status']) {
-    if (doc['data']['designer']) {
-      doc['data']['designer_suggestions'] = doc['data']['designer'];
-    }
+  //adds designer suggestions
+  if (doc['data']['designer']) {
+    doc['data']['designer_suggestions'] = doc['data']['designer'];
+  }
 
-    if (doc['data']['name']) {
-      suggestions = doc['data']['name'].split(' ');
+  // adds every word as an input to suggesters, stop words will be discarded by the stop analyzer i plugged in
+  if (doc['data']['name']) {
+    suggestions = doc['data']['name'].split(' ');
 
-      doc['data']['name_suggestions'] = suggestions;
-    }
+    doc['data']['name_suggestions'] = suggestions;
   }
 
   return doc;
