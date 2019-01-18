7395a6d47328b349fad54af4d40af5dec09230a2
shsangha <sssangha@ucalgary.ca>
2019-01-17 17:59:23 -0700

    now that using .raw for the fields i want to have autocomplete search enabled no longer need to add aditional info to the index

--- services/mongo-es-transport/pipeline.js
+++ services/mongo-es-transport/pipeline.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-var */
 var source = mongodb({
   uri: 'mongodb://root:root@listing-db:27017/listings?authSource=admin',
   tail: true,
@@ -13,5 +14,4 @@ var sink = elasticsearch({
 t.Source('source', source, '/.*/')
 .Transform(omit({"fields": ["description", "photos", "accept_offers", "comments", "buy_now", "shipping"]}))
 .Transform(goja({"filename":"priceFilterTransform.js"}))
-.Transform(goja({"filename": "indexSuggestions.js"}))
 .Save('sink', sink, '/.*/');
