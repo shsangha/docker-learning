b6a4e1f601b8043666d8086f26bc5c4af9892d90
shsangha <sssangha@ucalgary.ca>
2019-01-17 18:02:01 -0700

    best way i could think of doing autocomplete bc es 5.x lacks duplication detection in the context suggester, aggs are the workaround for duplication detection, msearch is to do the search in one rt

---
+++ services/listings/src/api/listings/resolvers/query/getSuggestions.js
@@ -0,0 +1,41 @@
+const getSuggestions = async (_, { input, size, sold }, { esClient }, info) => {
+  const formatQuery = field => {
+    const status = sold ? 'sold' : 'available';
+
+    return {
+      size: 0,
+      query: {
+        bool: {
+          must: [{ match: { [`${field}`]: input } }],
+          must_not: [{ term: { status } }]
+        }
+      },
+      aggs: {
+        aggregated_results: {
+          terms: {
+            field: `${field}.raw`,
+            size,
+            order: {
+              max_score: 'desc'
+            }
+          },
+          aggs: {
+            top_hit: { top_hits: { size: 1 } },
+            max_score: { max: { script: '_score' } }
+          }
+        }
+      }
+    };
+  };
+
+  const searchResults = await esClient.msearch({
+    body: [
+      { index: 'listings', type: 'listings' },
+      formatQuery('designer'),
+      { index: 'listings', type: 'listings' },
+      formatQuery('name')
+    ]
+  });
+};
+
+module.exports = getSuggestions;
