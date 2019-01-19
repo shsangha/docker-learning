cd76d0094a2528312164fbf340bb1778169ade2b
shsangha <sssangha@ucalgary.ca>
2019-01-18 21:18:39 -0700

    needed additional dependencies to be able to get babel7 to work with jest

--- services/web-app/jest.config.js
+++ services/web-app/jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.scss$': 'identity-obj-proxy',
+    '\\.module\\.scss$': 'identity-obj-proxy'
   },
-  setupFiles: 'jest-localstorage-mock',
-  collectCoverageFrom: [
-    '**/src/**/*.js(x)',
-  ],
+  setupFiles: ['jest-localstorage-mock'],
+  setupTestFrameworkScriptFile: '<rootDir>enzyme.config.js',
+  collectCoverageFrom: ['**/src/**/*.js(x)']
 };
