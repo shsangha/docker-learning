0d9d4edc67b4ed8d51292b155b436d1446574d94
shsangha <sssangha@ucalgary.ca>
2019-01-24 14:58:59 -0700

    realized i could just css for everything i wanted the searchContext to do so i just got rid of it

--- services/web-app/src/components/HeaderPrimary/index.jsx
+++ services/web-app/src/components/HeaderPrimary/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import styles from './style.module.scss';
-import SearchBar from './SearchBar';
 import Logo from './Logo';
 import HeaderMenu from './HeaderMenu';
-import { SearchContextProvider } from './SearchContext';
+import SearchBar from './SearchBar';
 
 export default () => (
-  <SearchContextProvider>
-    <header className={styles.globalHeader}>
-      <div className={styles.wrapper}>
-        <SearchBar />
-        <Logo />
-        <HeaderMenu />
-      </div>
-    </header>
-  </SearchContextProvider>
+  <header className={styles.globalHeader}>
+    <div className={styles.wrapper}>
+      <SearchBar />
+      <Logo />
+      <HeaderMenu />
+    </div>
+  </header>
 );
