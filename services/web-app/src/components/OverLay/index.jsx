f92a7671cde46c11b513496efbb7ab17482474db
shsangha <sssangha@ucalgary.ca>
2019-01-29 21:43:37 -0700

    moved className logic into parent so that style can be controlled easier, moved all resubable base styles to sass mixins/extends as appropriate

--- services/web-app/src/components/OverLay/index.jsx
+++ services/web-app/src/components/OverLay/index.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import styles from './style.module.scss';
 
-const Overlay = ({ onClick, transitionStatus, fullScreen }) => {
-  let classes;
-  if (fullScreen) {
-    classes = `${styles.overlay} ${styles[transitionStatus]}`;
-  } else {
-    classes = `${styles.overlay} ${styles.exposeHeader} ${styles[transitionStatus]}`;
-  }
-
+const Overlay = ({ onClick, classNames }) => {
   // divs aren't tabbable doesn't make sense for there to be a key event handler in this case, will allow keyboard users to exit with the button or esc key instead
   // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-  return <div role="presentation" onClick={onClick} className={classes} />;
+  return <div role="presentation" onClick={onClick} className={classNames} />;
 };
 
 Overlay.propTypes = {
   onClick: propTypes.func,
-  transitionStatus: propTypes.string.isRequired,
-  fullScreen: propTypes.bool
+  // fullScreen: propTypes.bool,
+  classNames: propTypes.string.isRequired
 };
 
 Overlay.defaultProps = {
-  onClick: () => {},
-  fullScreen: true
+  onClick: () => {}
+  // fullScreen: true
 };
 
 export default Overlay;
