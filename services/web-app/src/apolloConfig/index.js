8eee40ee9796dcdb249494de3642648765316029
shsangha <sssangha@ucalgary.ca>
2019-02-09 20:57:13 -0700

    moved logic from http link into seperate links, now creating the chain in the index

--- services/web-app/src/apolloConfig/index.js
+++ services/web-app/src/apolloConfig/index.js
@@ -1,10 +1,17 @@
 import { ApolloClient } from 'apollo-client';
+import { from } from 'apollo-link';
 
-import httpLink from './httpLink';
+import httpLink from './links/httpLink';
+import stateLink from './links/stateLink';
+import errorLink from './links/errorLink';
+import middlewareLink from './links/middlewareLink';
+import afterwareLink from './links/afterwareLink';
 import cache from './cache';
 
+const link = from([afterwareLink, middlewareLink, errorLink, stateLink, httpLink]);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache
 });
 
