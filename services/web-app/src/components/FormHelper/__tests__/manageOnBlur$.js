ba086a33908c3f0e95c64373954ba9bd47a5c49b
shsangha <sssangha@ucalgary.ca>
2019-03-04 14:51:14 -0700

    added test to make sure fields were set to touched correctly in the pipeline

--- services/web-app/src/components/FormHelper/__tests__/manageOnBlur$.js
+++ services/web-app/src/components/FormHelper/__tests__/manageOnBlur$.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { shallow } from 'enzyme';
-import { of, NEVER } from 'rxjs';
-import { share, tap } from 'rxjs/operators';
+import { of, NEVER, VirtualTimeScheduler } from 'rxjs';
+import { share, tap, concatMap, delay } from 'rxjs/operators';
 import { observe } from 'rxjs-marbles/jest';
 import FormHelper from '../index';
 
@@ -12,14 +12,73 @@ describe('tests that data flowing through the blur$ is handled as expected', () 
     jest.restoreAllMocks();
   });
 
+  test('sets the state of the field to touched if it was not already touched', done => {
+    const wrapper = shallow(<FormHelper>{() => {}}</FormHelper>);
+    const instance = wrapper.instance();
+    const name = 'field';
+    const fakeOnBlur = of({ name, value: 'fake' }).pipe(share());
+    const fakeOnSubmit = NEVER;
+
+    const touchedSpy = jest.spyOn(instance, 'setTouched');
+
+    instance.manageOnBlur$(fakeOnBlur, fakeOnSubmit).subscribe({
+      complete() {
+        try {
+          expect(touchedSpy).toHaveBeenCalledTimes(1);
+          expect(touchedSpy).toHaveBeenCalledWith(name);
+          done();
+        } catch (error) {
+          done.fail(error);
+        }
+      }
+    });
+  });
+
+  test('inner obvservable can be interupted with takeUntil when submit triggerd', done => {
+    const scheduler = new VirtualTimeScheduler();
+
+    const wrapper = shallow(<FormHelper>{() => {}}</FormHelper>);
+    const instance = wrapper.instance();
+    const fakeOnBlur = of(
+      { name: 'any', value: 'any' } // should run
+    ).pipe(share());
+
+    const fakeOnSubmit = of(1).pipe(concatMap(x => of(x).pipe(delay(100, scheduler))));
+
+    instance.runFieldLevelValidation = jest.fn(() =>
+      of({ doesnt: 'matter' }).pipe(delay(150, scheduler))
+    );
+
+    const start = scheduler.now();
+
+    const nextCalled = jest.fn();
+
+    instance.manageOnBlur$(fakeOnBlur, fakeOnSubmit).subscribe(
+      () => {
+        nextCalled();
+      },
+      () => {},
+      () => {
+        try {
+          expect(nextCalled).toHaveBeenCalledTimes(0);
+          done();
+        } catch (e) {
+          done.fail(e);
+        }
+      }
+    );
+
+    scheduler.flush();
+  });
+
   test(
     'returns the merged results from field and form level validation if no error is thrown',
     observe(() => {
       const wrapper = shallow(<FormHelper>{() => {}}</FormHelper>);
       const instance = wrapper.instance();
       const name = 'field';
-      const fakeOnChange = of({ name, value: 'fake' }).pipe(share());
-      const fakeOnBlur = NEVER;
+      const fakeOnBlur = of({ name, value: 'fake' }).pipe(share());
+      const fakeOnSubmit = NEVER;
 
       const fieldLevelSpy = jest
         .spyOn(instance, 'runFieldLevelValidation')
@@ -28,7 +87,7 @@ describe('tests that data flowing through the blur$ is handled as expected', () 
       const formLevelSpy = jest
         .spyOn(instance, 'runFormLevelValidation')
         .mockImplementation(() => of({ form: 'value' }));
-      return instance.manageOnBlur$(fakeOnChange, fakeOnBlur).pipe(
+      return instance.manageOnBlur$(fakeOnBlur, fakeOnSubmit).pipe(
         tap(combinedObj => {
           expect(combinedObj).toMatchObject({ form: 'value', field: { test: 'value' } });
           expect(fieldLevelSpy).toHaveBeenCalledTimes(1);
@@ -38,14 +97,46 @@ describe('tests that data flowing through the blur$ is handled as expected', () 
     })
   );
 
+  test('removes the field from the errors object if there is no error', done => {
+    const wrapper = shallow(<FormHelper>{() => {}}</FormHelper>);
+    const instance = wrapper.instance();
+    const name = 'test';
+    const fakeOnBlur = of({ name, value: 'fake' }).pipe(share());
+    const fakeOnSubmit = NEVER;
+
+    instance.runFieldLevelValidation = jest.fn(() => of(null));
+    instance.runFormLevelValidation = jest.fn(() => of({ root: 'error' }));
+
+    wrapper.setState(
+      {
+        errors: { test: 'some error' }
+      },
+      () => {
+        instance.manageOnBlur$(fakeOnBlur, fakeOnSubmit).subscribe({
+          next(x) {
+            try {
+              expect(x).toMatchObject({ root: 'error' });
+            } catch (e) {
+              done.fail(e);
+            }
+          },
+          error() {},
+          complete() {
+            done();
+          }
+        });
+      }
+    );
+  });
+
   test(
     'works with async validation',
     observe(() => {
       const wrapper = shallow(<FormHelper>{() => {}}</FormHelper>);
       const instance = wrapper.instance();
       const name = 'field';
-      const fakeOnChange = of({ name, value: 'fake' }).pipe(share());
-      const fakeOnBlur = NEVER;
+      const fakeOnBlur = of({ name, value: 'fake' }).pipe(share());
+      const fakeOnSubmit = NEVER;
 
       const fieldLevelSpy = jest
         .spyOn(instance, 'runFieldLevelValidation')
@@ -54,7 +145,7 @@ describe('tests that data flowing through the blur$ is handled as expected', () 
       const formLevelSpy = jest
         .spyOn(instance, 'runFormLevelValidation')
         .mockImplementation(() => new Promise(resolve => resolve({ form: 'value' })));
-      return instance.manageOnBlur$(fakeOnChange, fakeOnBlur).pipe(
+      return instance.manageOnBlur$(fakeOnBlur, fakeOnSubmit).pipe(
         tap(combinedObj => {
           expect(combinedObj).toMatchObject({ form: 'value', field: { test: 'value' } });
           expect(fieldLevelSpy).toHaveBeenCalledTimes(1);
@@ -74,8 +165,8 @@ describe('tests that data flowing through the blur$ is handled as expected', () 
       const wrapper = shallow(<FormHelper>{() => {}}</FormHelper>);
       const instance = wrapper.instance();
       const name = 'test';
-      const fakeOnChange = of({ name, value: 'fake' }).pipe(share());
-      const fakeOnBlur = NEVER;
+      const fakeOnBlur = of({ name, value: 'fake' }).pipe(share());
+      const fakeOnSubmit = NEVER;
 
       const spy = jest.spyOn(instance, 'runFieldLevelValidation').mockImplementation(() => {
         throw new Error('error');
@@ -83,7 +174,7 @@ describe('tests that data flowing through the blur$ is handled as expected', () 
 
       const setErrSpy = jest.spyOn(instance, 'setFormLevelError');
 
-      return instance.manageOnBlur$(fakeOnChange, fakeOnBlur).pipe(
+      return instance.manageOnBlur$(fakeOnBlur, fakeOnSubmit).pipe(
         tap(error => {
           expect(error).toMatchObject(wrapper.state().errors);
           expect(setErrSpy).toHaveBeenCalledTimes(1);
