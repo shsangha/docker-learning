1446d88e97e85791c3540f09c46dd298f2cd3b5b
shsangha <sssangha@ucalgary.ca>
2019-03-12 22:29:34 -0600

    stubbed out dependencies on other functions some more for more black box testing so that these tests werent relying on the implementation of other functions

--- services/web-app/src/components/SuggestionSearch/__tests__/unit/calculateHighlightedIndex.js
+++ services/web-app/src/components/SuggestionSearch/__tests__/unit/calculateHighlightedIndex.js
@@ -2,95 +2,77 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-undef */
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import SuggestionSearch from '../../../SuggestionSearch';
 
 const defaultSuggestions = ['one', 'two', 'three', 'four', 'five', 'six'];
 
 describe('tests for the calculateHighlightedIndex method', () => {
   test('return -1 if the menu is not open', () => {
-    const wrapper = mount(
-      <SuggestionSearch defaultSuggestions={defaultSuggestions}>
-        {({ getInputProps }) => <input {...getInputProps()} />}
-      </SuggestionSearch>
-    );
+    const wrapper = shallow(<SuggestionSearch>{jest.fn()}</SuggestionSearch>);
     const instance = wrapper.instance();
+    instance.getMergedState = jest.fn(() => ({
+      open: false,
+      highlightedIndex: 0,
+      suggestions: ['some', 'fake', 'suggestions']
+    }));
     const highlightedIndex = instance.calculateHighlightedIndex(0);
     expect(highlightedIndex).toEqual(-1);
-    wrapper.unmount();
   });
   test('returns 0 if we exceed the highlightedIndex', () => {
-    const wrapper = mount(
-      <SuggestionSearch
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={5}
-        openByDefault
-      >
-        {({ getInputProps }) => <input {...getInputProps()} />}
-      </SuggestionSearch>
-    );
+    const wrapper = shallow(<SuggestionSearch>{jest.fn()}</SuggestionSearch>);
     const instance = wrapper.instance();
+    instance.getMergedState = jest.fn(() => ({
+      open: true,
+      highlightedIndex: 2,
+      suggestions: ['some', 'fake', 'suggestions']
+    }));
     const highlightedIndex = instance.calculateHighlightedIndex(1);
     expect(highlightedIndex).toEqual(0);
-    wrapper.unmount();
   });
   test('returns the last index if we move below 0', () => {
-    const wrapper = mount(
-      <SuggestionSearch
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={0}
-        openByDefault
-      >
-        {({ getInputProps }) => <input {...getInputProps()} />}
-      </SuggestionSearch>
-    );
+    const wrapper = shallow(<SuggestionSearch>{jest.fn()}</SuggestionSearch>);
+
     const instance = wrapper.instance();
+    instance.getMergedState = jest.fn(() => ({
+      open: true,
+      highlightedIndex: 0,
+      suggestions: ['some', 'fake', 'suggestions']
+    }));
     const highlightedIndex = instance.calculateHighlightedIndex(-1);
-    expect(highlightedIndex).toEqual(5);
+    expect(highlightedIndex).toEqual(2);
   });
   test('edge case where new index is 0 should just return 0', () => {
-    const wrapper = mount(
-      <SuggestionSearch
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={1}
-        openByDefault
-      >
-        {({ getInputProps }) => <input {...getInputProps()} />}
-      </SuggestionSearch>
-    );
+    const wrapper = shallow(<SuggestionSearch>{jest.fn()}</SuggestionSearch>);
     const instance = wrapper.instance();
+    instance.getMergedState = jest.fn(() => ({
+      open: true,
+      highlightedIndex: 1,
+      suggestions: ['some', 'fake', 'suggestions']
+    }));
     const highlightedIndex = instance.calculateHighlightedIndex(-1);
     expect(highlightedIndex).toEqual(0);
-    wrapper.unmount();
   });
   test('when starting from -1(menu closed or no highlight) we move to 0', () => {
-    const wrapper = mount(
-      <SuggestionSearch
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={-1}
-        openByDefault
-      >
-        {({ getInputProps }) => <input {...getInputProps()} />}
-      </SuggestionSearch>
-    );
+    const wrapper = shallow(<SuggestionSearch>{jest.fn()}</SuggestionSearch>);
     const instance = wrapper.instance();
+    instance.getMergedState = jest.fn(() => ({
+      open: true,
+      highlightedIndex: -1,
+      suggestions: ['some', 'fake', 'suggestions']
+    }));
     const highlightedIndex = instance.calculateHighlightedIndex(1);
     expect(highlightedIndex).toEqual(0);
-    wrapper.unmount();
   });
   test('edge case - last item should return last index instead of 0', () => {
-    const wrapper = mount(
-      <SuggestionSearch
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={4}
-        openByDefault
-      >
-        {({ getInputProps }) => <input {...getInputProps()} />}
-      </SuggestionSearch>
-    );
+    const wrapper = shallow(<SuggestionSearch>{jest.fn()}</SuggestionSearch>);
     const instance = wrapper.instance();
+    instance.getMergedState = jest.fn(() => ({
+      open: true,
+      highlightedIndex: 1,
+      suggestions: ['some', 'fake', 'suggestions']
+    }));
     const highlightedIndex = instance.calculateHighlightedIndex(1);
-    expect(highlightedIndex).toEqual(5);
-    wrapper.unmount();
+    expect(highlightedIndex).toEqual(2);
   });
 });
