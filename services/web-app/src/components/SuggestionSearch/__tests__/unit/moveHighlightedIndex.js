c40d46c6bcf88cac633072cc41de176b92d38088
shsangha <sssangha@ucalgary.ca>
2019-03-14 14:08:13 -0600

    realized some of these tests were dependent upon the implementation of other functions, fixed issues and cleaned up cleanup logic for mocks

--- services/web-app/src/components/SuggestionSearch/__tests__/unit/moveHighlightedIndex.js
+++ services/web-app/src/components/SuggestionSearch/__tests__/unit/moveHighlightedIndex.js
@@ -8,6 +8,10 @@ import changeTypes from '../../../SuggestionSearch/changeTypes';
 const defaultSuggestions = ['one', 'two', 'three', 'four', 'five', 'six'];
 
 describe('tests for the moveHighlightedIndex method', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
   test('prevents default on the event', () => {
     const wrapper = shallow(<SuggestionSearch>{() => {}}</SuggestionSearch>);
     const instance = wrapper.instance();
@@ -23,30 +27,35 @@ describe('tests for the moveHighlightedIndex method', () => {
     const calcSpy = jest.spyOn(instance, 'calculateHighlightedIndex');
     instance.moveHighlightedIndex(1, changeTypes.inputChange, { preventDefault: () => {} });
     expect(calcSpy).toHaveBeenCalledTimes(1);
-    calcSpy.mockRestore();
   });
-  test('calls set state', () => {
+  test('calls internalSetState as expected', () => {
     const wrapper = shallow(
       <SuggestionSearch
         openByDefault
         highlightedOnStart={3}
         defaultSuggestions={defaultSuggestions}
       >
-        {() => {}}
+        {({ getInputProps }) => <input {...getInputProps()} />}
       </SuggestionSearch>
     );
     const instance = wrapper.instance();
     const setStateSpy = jest.spyOn(instance, 'internalSetState');
+    const mergedStateSpy = jest.spyOn(instance, 'getMergedState').mockImplementation(() => ({
+      inputValue: 'any',
+      suggestions: ['first', 'second', 'third']
+    }));
+    const calculateIndexSpy = jest
+      .spyOn(instance, 'calculateHighlightedIndex')
+      .mockImplementation(() => 1);
     instance.moveHighlightedIndex(1, changeTypes.inputChange, { preventDefault: () => {} });
     expect(setStateSpy).toHaveBeenCalledTimes(1);
     expect(setStateSpy).toHaveBeenCalledWith(
       {
-        highlightedIndex: 4,
-        inputValue: 'five',
+        highlightedIndex: 1,
+        inputValue: 'second',
         open: true
       },
       changeTypes.inputChange
     );
-    setStateSpy.mockRestore();
   });
 });
