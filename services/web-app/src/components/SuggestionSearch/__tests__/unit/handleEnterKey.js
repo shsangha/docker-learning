3a41fb99f8d24cd84140e52c831458bc787a4d4f
shsangha <sssangha@ucalgary.ca>
2019-03-15 13:47:41 -0600

    was using restoreAllMocks instead of resetAllMocks so counts were getting messed up, also made tests less dependent on implementation details of other functions

--- services/web-app/src/components/SuggestionSearch/__tests__/unit/handleEnterKey.js
+++ services/web-app/src/components/SuggestionSearch/__tests__/unit/handleEnterKey.js
@@ -9,7 +9,6 @@ import changeTypes from '../../changeTypes';
 const defaultSuggestions = ['one', 'two', 'three', 'four', 'five', 'six'];
 
 describe('tests for when the enterKey method is invoked', () => {
-  const fakeOnSelect = jest.fn();
   const fakeEvent = {
     preventDefault: jest.fn()
   };
@@ -18,92 +17,79 @@ describe('tests for when the enterKey method is invoked', () => {
   });
 
   test('does nothing if there isnt no current suggetion and menu is open', () => {
-    const wrapper = shallow(
-      <SuggestionSearch
-        open
-        onSelect={fakeOnSelect}
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={-1}
-      >
-        {() => {}}
-      </SuggestionSearch>
-    );
+    const wrapper = shallow(<SuggestionSearch onSelect={jest.fn()}>{jest.fn()}</SuggestionSearch>);
     const instance = wrapper.instance();
 
     const internalSetStateSpy = jest.spyOn(instance, 'internalSetState');
-    instance.handleEnterKey(fakeEvent);
-    expect(fakeEvent.preventDefault).toHaveBeenCalledTimes(0);
-    expect(instance.props.onSelect).toBeCalledTimes(0);
-    expect(internalSetStateSpy).toBeCalledTimes(0);
-  });
-  test('does nothing if the menu isnt open and there is a higlighted item (should never actually happen unless we do that in a state reducer for some reason)', () => {
-    const wrapper = mount(
-      <SuggestionSearch
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={1}
-        onSelect={fakeOnSelect}
-      >
-        {({ itemRef }) => (
-          <div>
-            <li ref={itemRef}>Fake item</li>
-          </div>
-        )}
-      </SuggestionSearch>
-    );
-    const instance = wrapper.instance();
-    const internalSetStateSpy = jest.spyOn(instance, 'internalSetState');
-    instance.handleEnterKey(fakeEvent);
-    expect(fakeEvent.preventDefault).toHaveBeenCalledTimes(0);
-    expect(instance.props.onSelect).toHaveBeenCalledTimes(0);
-    expect(internalSetStateSpy).toBeCalledTimes(0);
-    wrapper.unmount();
-  });
-  test('does nothing if the highlighted item is disabled', () => {
-    const wrapper = mount(
-      <SuggestionSearch
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={1}
-        onSelect={fakeOnSelect}
-        openByDefault
-      >
-        {({ itemRef }) => (
-          <div>
-            <li disabled ref={itemRef}>
-              Fake item
-            </li>
-          </div>
-        )}
-      </SuggestionSearch>
-    );
-    const instance = wrapper.instance();
-    const internalSetStateSpy = jest.spyOn(instance, 'internalSetState');
+    instance.getMergedState = jest.fn(() => ({
+      open: true,
+      highlightedIndex: 0,
+      suggestions: []
+    }));
+    instance.highlightedRef = {
+      current: {
+        hasAttribute: jest.fn(() => false)
+      }
+    };
     instance.handleEnterKey(fakeEvent);
     expect(fakeEvent.preventDefault).toHaveBeenCalledTimes(1);
-    expect(instance.props.onSelect).toHaveBeenCalledTimes(0);
-    expect(internalSetStateSpy).toHaveBeenCalledTimes(0);
+    expect(instance.props.onSelect).not.toHaveBeenCalled();
+    expect(internalSetStateSpy).not.toHaveBeenCalled();
   });
-  test('calls internalSetState properly if passing all checks', () => {
-    const wrapper = mount(
-      <SuggestionSearch
-        defaultSuggestions={defaultSuggestions}
-        highlightedOnStart={1}
-        onSelect={fakeOnSelect}
-        openByDefault
-      >
-        {({ itemRef }) => (
-          <div>
-            <li ref={itemRef}>Fake item</li>
-          </div>
-        )}
-      </SuggestionSearch>
-    );
+  test('does nothing if the menu isnt open and there is a higlighted item (should never actually happen unless we do that in a state reducer for some reason)', () => {
+    const wrapper = shallow(<SuggestionSearch onSelect={jest.fn()}>{jest.fn()}</SuggestionSearch>);
     const instance = wrapper.instance();
     const internalSetStateSpy = jest.spyOn(instance, 'internalSetState');
+    instance.getMergedState = jest.fn(() => ({
+      open: false,
+      highlightedIndex: 0,
+      suggestions: ['uno']
+    }));
+    instance.highlightedRef = {
+      current: {
+        hasAttribute: jest.fn(() => false)
+      }
+    };
     instance.handleEnterKey(fakeEvent);
-    expect(instance.props.onSelect).toHaveBeenCalledTimes(1);
+    expect(fakeEvent.preventDefault).toHaveBeenCalledTimes(1);
+    expect(instance.props.onSelect).not.toHaveBeenCalled();
+    expect(internalSetStateSpy).not.toHaveBeenCalled();
+  });
+  test('does nothing if the highlighted item is disabled', () => {
+    const wrapper = shallow(<SuggestionSearch onSelect={jest.fn()}>{() => {}}</SuggestionSearch>);
+    const instance = wrapper.instance();
+    const internalSetStateSpy = jest.spyOn(instance, 'internalSetState');
+    instance.getMergedState = jest.fn(() => ({
+      open: true,
+      highlightedIndex: 0,
+      suggestions: ['first', 'second', 'third']
+    }));
+    instance.highlightedRef = {
+      current: {
+        hasAttribute: jest.fn(() => true)
+      }
+    };
+    instance.handleEnterKey(fakeEvent);
+    expect(internalSetStateSpy).not.toHaveBeenCalled();
+    expect(instance.props.onSelect).not.toHaveBeenCalled();
+  });
+  test('calls internalSetState properly if passing all checks', () => {
+    const wrapper = shallow(<SuggestionSearch onSelect={jest.fn()}>{() => {}}</SuggestionSearch>);
+    const instance = wrapper.instance();
+    const internalSetStateSpy = jest.spyOn(instance, 'internalSetState');
+    instance.getMergedState = jest.fn(() => ({
+      open: true,
+      highlightedIndex: 0,
+      suggestions: ['first', 'second', 'third']
+    }));
+    instance.highlightedRef = {
+      current: {
+        hasAttribute: jest.fn(() => false)
+      }
+    };
+    instance.handleEnterKey(fakeEvent);
     expect(internalSetStateSpy).toHaveBeenCalledTimes(1);
     expect(internalSetStateSpy).toHaveBeenCalledWith({ open: false }, changeTypes.itemSelected);
-    expect(instance.props.onSelect).toBeCalledWith('two');
-    wrapper.unmount();
+    expect(instance.props.onSelect).toBeCalledWith('first');
   });
 });
