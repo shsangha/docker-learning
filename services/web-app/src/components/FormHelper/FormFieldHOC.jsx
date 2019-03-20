820488c0bfcdcf26a37da0da33ceac9cce490c93
shsangha <sssangha@ucalgary.ca>
2019-03-19 18:25:31 -0600

    caught bug where internal values where always returning undefined, problem was name was being read from context when it was a prop

--- services/web-app/src/components/FormHelper/FormFieldHOC.jsx
+++ services/web-app/src/components/FormHelper/FormFieldHOC.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import { FormContext } from './index';
+import pipe from './utils/pipe';
+import isEmptyObj from './utils/isEmptyObj';
 
-export default Component =>
+export default (Component, multiField) =>
   class FormFieldHoc extends React.Component {
     static contextType = FormContext;
 
@@ -19,10 +21,31 @@ export default Component =>
       validateOnBlur: true,
       dynamic: false
     };
+
+    pipeMultiFieldValidation = (validator, name) => {
+      return pipe(
+        validator,
+        err => (isEmptyObj(err) ? null : err),
+        filterdError =>
+          filterdError
+            ? setInternvalError(
+                retrieveInternalValue(this.context.errors, name) || {},
+                toPath(name).slice(-1),
+                err
+              )
+            : {}
+      );
+    };
+
     componentDidMount() {
       const { name, validateOnChange, validateOnBlur, validator } = this.props;
       if (validator) {
-        this.context.attachFieldValidator(name, validator, validateOnChange, validateOnBlur);
+        this.context.attachFieldValidator(
+          name,
+          multiField ? pipeMultiFieldValidation(validator, name) : validator,
+          validateOnChange,
+          validateOnBlur
+        );
       }
     }
     componentWillUnmount() {
@@ -35,6 +58,17 @@ export default Component =>
     }
 
     render() {
-      return <Component {...this.props} {...this.context} />;
+      const { retrieveInternalValue, values, errors, touched } = this.context;
+      const { name } = this.props;
+
+      console.log(values, name, 'VALUES');
+
+      const FieldState = {
+        value: retrieveInternalValue(values, name),
+        errors: retrieveInternalValue(errors, name),
+        touched: retrieveInternalValue(touched, name)
+      };
+
+      return <Component {...this.props} {...this.context} FieldState={FieldState} />;
     }
   };
