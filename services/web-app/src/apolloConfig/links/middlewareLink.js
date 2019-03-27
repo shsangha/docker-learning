c7738f6da5b6b72514c09803c3de1175ecf89d3c
shsangha <sssangha@ucalgary.ca>
2019-03-27 14:10:46 -0600

    localstorage always returns a string so returning null would result in a truthy value when it should not be, replaced returning  null with the empty string in the case where no token exists to prevent the auth middlware from always detecting the existence of a token

--- services/web-app/src/apolloConfig/links/middlewareLink.js
+++ services/web-app/src/apolloConfig/links/middlewareLink.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
-import { setContext } from 'apollo-link-context';
+import { setContext } from "apollo-link-context";
 
 // prettier-ignore
 const middlewareLink = setContext((_, { headers }) => ({
       headers: {
         ...headers,
-        "x-token": localStorage.getItem('token') || null,
-        "x-refreshToken": localStorage.getItem('refreshToken')|| null
+        "x-token": localStorage.getItem('token') || '',
+        "x-refreshToken": localStorage.getItem('refreshToken')|| ''
       }
     }));
 
