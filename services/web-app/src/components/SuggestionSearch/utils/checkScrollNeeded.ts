c772c64d89b5ca55d5bf54f6f506abca87ce7b2c
shsangha <sssangha@ucalgary.ca>
2019-03-29 03:52:16 -0600

    converted changeTypes into an enum and converted other helpers to ts

--- services/web-app/src/components/SuggestionSearch/utils/checkScrollNeeded.ts
+++ services/web-app/src/components/SuggestionSearch/utils/checkScrollNeeded.ts
@@ -1,8 +1,8 @@
 /* @input {DOMnode} menu - the current ref attached to the menu
-  *  @input {DOMNODE} item - the currently selected item in the menu
-  *  @return {boolean} returns true if any part of the item is not in view
-  */
-export default function(menu, item) {
+ *  @input {DOMNODE} item - the currently selected item in the menu
+ *  @return {boolean} returns true if any part of the item is not in view
+ */
+export default function(menu: HTMLElement, item: HTMLElement) {
   const visibleMenuTop = menu.scrollTop;
   const visibleMenuBottom = menu.clientHeight + visibleMenuTop;
 
@@ -10,5 +10,8 @@ export default function(menu, item) {
   const highlightedItemBottom = item.clientHeight + highlightedItemTop;
 
   // checks to see if any portion of the item isnt visible
-  return highlightedItemTop < visibleMenuTop || highlightedItemBottom > visibleMenuBottom;
+  return (
+    highlightedItemTop < visibleMenuTop ||
+    highlightedItemBottom > visibleMenuBottom
+  );
 }
---
+++ services/web-app/src/components/SuggestionSearch/utils/combineEventHandlers.ts
@@ -0,0 +1,17 @@
+/* Function that takes in a list of event handlers and runs them in order unless
+ *  preventInternalEventHandlers is set in which case it prevents all remaining event handlers frim running
+ *
+ *  @param {[function]} fns - a list of functions that we want to run one after another
+ *  @return - a function that accepts an event and args and runs each function with the event/args
+ */
+export default function(...fns: Array<(e: any, ...args: any[]) => any>) {
+  return (e: any, ...args: any[]) => {
+    fns.some(fn => {
+      if (fn && typeof fn === "function") {
+        // @ts-ignore needed to do this because of a bug with spread args https://github.com/Microsoft/TypeScript/issues/4130
+        fn(e, ...args);
+      }
+      return e.preventInternalEventHandlers;
+    });
+  };
+}
